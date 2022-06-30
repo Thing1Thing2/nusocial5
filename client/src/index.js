fd803f1078406eef79e6d3c152dbf03498b3acd5
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-06-30 14:13:46 +0530

    LaunchPage.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import './index.css';
-import App from './App';
-import LaunchPage from './LaunchPage';
-
+import LaunchPage from './pages/LaunchPage/LaunchPage';
+import Home from './pages/Home/Home';
+import PersonalChat from "./pages/PersonalChat/PersonalChat"
+import QuickLinks from "./pages/QuickLinks/QuickLinks"
+import Profile from "./pages/Profile/Profile";
+import NewsAndNots from './pages/NewsAndNots/NewsAndNots';
+import Group from './pages/Group/Group';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
   <Routes>
-     <Route path = "/" element = {<App />} />
-  <Route path = "/LaunchPage" element = { <LaunchPage />} />
+  <Route path = "/" element = {<LaunchPage />} />
+  <Route path = "/home" element = { <Home />} />
+  <Route path = "/personalchat" element = {<PersonalChat />} />
+  <Route path = "/quicklinks" element = {<QuickLinks />} />
+  <Route path = "/profile" element = {<Profile />} />
+  <Route path = "/newsandnots" element = {<NewsAndNots />} />
+  <Route path = "/group" element = {<Group />} />
   </Routes>
   </BrowserRouter>
 

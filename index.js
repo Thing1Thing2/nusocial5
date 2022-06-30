d659b70af6e8fbc268ecfd59d1484d7d5da9b124
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-06-30 18:17:19 +0530

    LaunchPage.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors')
 const http = require("http");
-
+const { Server } = require("socket.io");
 const app = express();
 
 // Serve static files from the React app
@@ -10,6 +10,35 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(cors());
+
+// middleware
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+      origin: "https://nusocial4.herokuapp.com",
+      methods: ["GET", "POST"],
+    },
+  });
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.chat).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("SERVER RUNNING");
+});
 
 // routers
 const router = require('./routes/studentRouter.js')

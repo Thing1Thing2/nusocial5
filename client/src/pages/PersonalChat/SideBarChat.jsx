6f3c92a2f6d9b72a74ed46e1c68c745f24d4e7b0
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-07-08 18:13:24 +0530

    live message sending

--- client/src/pages/PersonalChat/SideBarChat.jsx
+++ client/src/pages/PersonalChat/SideBarChat.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./sideBarChat.css";
 import { Avatar } from "@mui/material";
 import "./sideBarChat.css";
 
-const SideBarChat = ({ chatName, clickAction, latestMessage }) => {
+const SideBarChat = ({ chatName, clickAction }) => {
   /*
     (1) Show friends list with whom there is no
         chat history
@@ -11,7 +11,31 @@ const SideBarChat = ({ chatName, clickAction, latestMessage }) => {
     (3) Or create emptyChat with only yourself in it
     (4) Make a group 
         */
+  const [latestMsg, setLatestMsg] = useState("latest message");
+  const latestMessage = async (chatId) => {
+    const info = {
+      chatId: chatId,
+    };
+    const settings = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(info),
+    };
+    let results = await fetch(
+      "https://nusocial5.herokuapp.com/api/personalchats/latestMessage",
+      settings
+    );
 
+    let arr = await results.json();
+
+    setLatestMsg(arr.message);
+  };
+  window.onload = () => {
+    latestMessage(chatName[2]);
+  };
   return (
     <div
       className="sidebarchat"
@@ -23,7 +47,7 @@ const SideBarChat = ({ chatName, clickAction, latestMessage }) => {
       <Avatar src={chatName[1]} />
       <div className="sidebarchat_info">
         <h2>{chatName[0]}</h2>
-        <p>{latestMessage}</p>
+        <p>{latestMsg}</p>
       </div>
     </div>
   );

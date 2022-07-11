09009094ab4e741af542ef16ec4d760dd6b33265
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-07-11 18:33:05 +0530

    load album images

--- client/src/components/ProfileMid/ProfileMid.jsx
+++ client/src/components/ProfileMid/ProfileMid.jsx
@@ -177,7 +177,7 @@ const ProfileMid = ({ username }) => {
   };
 
   const [Images, setImages] = useState([]);
-  const albumImages = () => {
+  const albumImages = async () => {
     const data = {
       username: username,
     };
@@ -199,65 +199,65 @@ const ProfileMid = ({ username }) => {
       setImages(imgs);
     });
 
-    const n = Images.length;
+    let n = await Images.length;
     console.log(n);
     ProfileAlbumList = [
       {
         img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
-        title: "Breakfast",
+        title: "Picture1",
         rows: 2,
         cols: 2,
       },
       {
         img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
-        title: "Burger",
+        title: "Picture2",
       },
       {
         img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
-        title: "Camera",
+        title: "Picture3",
       },
       {
         img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
-        title: "Coffee",
+        title: "Picture4",
         cols: 2,
       },
       {
         img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
-        title: "Hats",
+        title: "Picture5",
         cols: 2,
       },
       {
         img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
-        title: "Honey",
+        title: "Picture6",
         author: "@arwinneil",
         rows: 2,
         cols: 2,
       },
       {
         img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
-        title: "Basketball",
+        title: "Picture7",
       },
       {
         img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
-        title: "Fern",
+        title: "Picture8",
       },
       {
         img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
-        title: "Mushrooms",
+        title: "Picture9",
         rows: 2,
         cols: 2,
       },
       {
         img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
-        title: "Tomato basil",
+        title: "Picture10",
       },
       {
         img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
-        title: "Sea star",
+        title: "Picture11",
       },
       {
         img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
-        title: "Bike",
+        title: "Picture12",
         cols: 2,
       },
     ];

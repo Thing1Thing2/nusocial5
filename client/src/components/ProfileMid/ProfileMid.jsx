de6da46814816555fed86153b7f7431610a07491
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-07-11 18:22:54 +0530

    load album images

--- client/src/components/ProfileMid/ProfileMid.jsx
+++ client/src/components/ProfileMid/ProfileMid.jsx
@@ -200,6 +200,67 @@ const ProfileMid = ({ username }) => {
     });
   };
 
+  const ProfileAlbumList = [
+    {
+      img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+      title: "Breakfast",
+      rows: 2,
+      cols: 2,
+    },
+    {
+      img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+      title: "Burger",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+      title: "Camera",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+      title: "Coffee",
+      cols: 2,
+    },
+    {
+      img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+      title: "Hats",
+      cols: 2,
+    },
+    {
+      img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+      title: "Honey",
+      author: "@arwinneil",
+      rows: 2,
+      cols: 2,
+    },
+    {
+      img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+      title: "Basketball",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+      title: "Fern",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+      title: "Mushrooms",
+      rows: 2,
+      cols: 2,
+    },
+    {
+      img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+      title: "Tomato basil",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+      title: "Sea star",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+      title: "Bike",
+      cols: 2,
+    },
+  ];
+
   return (
     <div className="profileMid">
       <div className="profileTopContainer">
@@ -255,10 +316,14 @@ const ProfileMid = ({ username }) => {
                 cols={4}
                 rowHeight={121}
               >
-                {Images.map((item) => (
-                  <ImageListItem key={item} cols={item || 1} rows={item || 1}>
+                {ProfileAlbumList.map((item) => (
+                  <ImageListItem
+                    key={item.img}
+                    cols={item.cols || 1}
+                    rows={item.rows || 1}
+                  >
                     <img
-                      {...srcset(item, 121, 0, 0)}
+                      {...srcset(item.img, 121, item.rows, item.cols)}
                       alt={item}
                       loading="lazy"
                     />

c2c2d7d05b1f881dde0f011e45e33b1dcca9125d
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-07-15 23:29:09 +0530

    search bar position to be able to see header title PLUS logo link

--- client/src/components/Logo/Logo.jsx
+++ client/src/components/Logo/Logo.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import "./logo.css";
 import logo from "./logo.png";
-import { Link } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
-const Logo = ({ link, title }) => {
+const Logo = ({ link, title, username }) => {
+  const navigate = useNavigate();
   return (
-    <div className="logoAndHeader">
-      <Link href={link} style={{ textDecoration: "none" }} s>
-        <div className="logo">
-          <img src={logo} alt="logo" className="logo" />
-        </div>
-        <div className="pageTitle">{title}</div>
-      </Link>
+    <div
+      className="logoAndHeader"
+      onClick={() =>
+        navigate(
+          { link },
+          {
+            state: { username: username },
+          }
+        )
+      }
+    >
+      <div className="logo">
+        <img src={logo} alt="logo" className="logo" />
+      </div>
+      <div className="pageTitle">{title}</div>
     </div>
   );
 };

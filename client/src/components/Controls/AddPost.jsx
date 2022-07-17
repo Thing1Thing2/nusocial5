8ea7dc8f5591682bd9381fc4081dcd258691757e
hhchinh2002 <64810838+hhchinh2002@users.noreply.github.com>
2022-07-18 03:06:19 +0800

    Update AddPost.jsx

--- client/src/components/Controls/AddPost.jsx
+++ client/src/components/Controls/AddPost.jsx
@@ -1,8 +1,14 @@
+import './addPost.css';
 import React, { useState } from "react";
 import { Alert } from "@mui/material";
+import { Avatar } from '@mui/material'
+import InsertPhotoIcon from '@mui/icons-material/InsertPhoto'
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon'
+import Picker from "emoji-picker-react"
 import Collapse from "@mui/material/Collapse";
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
+
 
 const AddPost = ({ username }) => {
   const [msg, setMsg] = useState("");
@@ -12,6 +18,10 @@ const AddPost = ({ username }) => {
     body: "",
     title: "",
   });
+  const [showPicker, setShowPicker] = useState(false);
+  const onEmojiClick = (event, emojiObj) => {
+    setShowPicker(false);
+}
   function submitPost(e) {
     setOpen(false);
     console.log("username given: " + username.toString());
@@ -21,7 +31,6 @@ const AddPost = ({ username }) => {
     formData.append("username", username);
     formData.append("image", fileField.files[0]);
     formData.append("body", postData.body);
-    formData.append("title", postData.title);
     const settings = {
       method: "POST",
       body: formData,
@@ -42,50 +51,60 @@ const AddPost = ({ username }) => {
 
   return (
     <div>
-      <div className="AddPost">
-        <Collapse in={open}>
-          <Alert
-            severity={severity}
-            action={
-              <IconButton
-                aria-label="close"
-                color="inherit"
-                size="small"
-                onClick={() => {
-                  setOpen(false);
-                }}
-              >
-                {" "}
-                <CloseIcon fontSize="inherit" />{" "}
-              </IconButton>
-            }
-            sx={{ mb: 2 }}
-          >
-            {msg}
-          </Alert>
-        </Collapse>
-        <form onSubmit={(e) => submitPost(e)}>
-          <input
-            type="text"
-            placeholder="Entre message title"
-            id="title"
-            onChange={(e) => handle(e)}
-          />
-          <input
-            type="text"
-            placeholder="Enter message body"
-            id="body"
-            onChange={(e) => handle(e)}
-          />
-          <input
-            type="file"
-            id="photo"
-            name="filename"
-            placeholder="upload post picture"
-          />
-          <input type="submit" placeholder="submit post" />
-        </form>
-      </div>
+      <Collapse in={open}>
+        <Alert
+          severity={severity}
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => {
+                setOpen(false);
+              }}
+            >
+              {" "}
+              <CloseIcon fontSize="inherit" />{" "}
+            </IconButton>
+          }
+          sx={{ mb: 2 }}
+        >
+          {msg}
+        </Alert>
+      </Collapse>
+      <div className="posting">
+            <div>
+                <div className="avatar">
+                    <Avatar src="https://is5-ssl.mzstatic.com/image/thumb/Purple113/v4/ec/83/3a/ec833a37-1e6f-958e-9e60-4f358795405f/source/512x512bb.jpg" />
+                </div>
+                <div className="postContextContainer">
+                    <input 
+                        className="postContext"
+                        placeholder="Write something to share with your friends here"
+                        onChange={
+                          (e) => handle(e)
+                        }
+                    />
+                </div>
+            </div>
+            <div className="postingBottom">
+                <div className="postingAdditional">
+                    <div className="postImage">
+                        <input type="file"/>
+                        <InsertPhotoIcon sx={{ fontSize: 40 }} className ="icon" />
+                    </div>
+                    <div className="postEmoji">
+                        <InsertEmoticonIcon sx={{ fontSize: 40 }} className="icon" onClick = {() => setShowPicker(val => !val)}/>
+                        <i>
+                            {showPicker && <Picker onEmojiClick={ onEmojiClick}/> }
+                        </i>
+                    </div>
+                </div>
+                <button className="postingButton" onClick={(e) => submitPost(e)}>
+                    Post
+                </button>
+            </div>  
+        </div>
     </div>
   );
 };

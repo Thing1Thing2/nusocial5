d1b5a2cb82bed0685bae2eeeefb8623e62c68bdf
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-07-18 23:37:30 +0530

    Update groupNamesRouter.js

--- routes/groupNamesRouter.js
+++ routes/groupNamesRouter.js
@@ -6,4 +6,6 @@ groupNamesRouter.post("/addGroupName", groupNamesController.addGroup);
 groupNamesRouter.post("/allGroups", groupNamesController.allGroups);
 groupNamesRouter.post("/allMyGroups", groupNamesController.allMyGroups);
 groupNamesRouter.post("/getGroupData", groupNamesController.getGroupData);
+groupNamesRouter.post("/getPublicGroups", groupNamesController.getPublicGroups);
+
 module.exports = groupNamesRouter;

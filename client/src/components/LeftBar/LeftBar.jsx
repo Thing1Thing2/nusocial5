8444ecce5ef70ab5cc514171d7f0c0338aed2017
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-07-18 23:46:35 +0530

    Update LeftBar.jsx

--- client/src/components/LeftBar/LeftBar.jsx
+++ client/src/components/LeftBar/LeftBar.jsx
@@ -8,6 +8,7 @@ import NewTag from "../Controls/NewTag";
 import AddEvent from "../Controls/AddEvent";
 
 const LeftBar = ({ username }) => {
+  let guest = username.includes("guest");
   const navigate = useNavigate();
 
   const [Groups, setGroups] = useState([]);
@@ -88,67 +89,108 @@ const LeftBar = ({ username }) => {
       setEvents(events);
     });
   };
-
-  return (
-    <div className="leftBar">
-      <div className="leftBarComponentContainer">
-        <CreateGroup username={username} />
-        <input
-          type="submit"
-          placeholder="refresh feed"
-          onClick={getAllGroups}
-          value="refresh suggestion"
-        />
-        <div className="containerTitle">Your Groups</div>
-        {Groups.map((u) => (
-          <div className="group">
-            <div className="groupAvatar">
-              <Avatar
-                src={u[1]}
-                onClick={() => navigate("/group", { state: { group: u[0] } })}
-              />
-            </div>
-            <div className="groupContainerRight">
-              <div className="groupName">{u[0]}</div>
-              <div className="groupDescription">{u[2]}</div>
-            </div>
-
-            <ViewGroup username={username} groupName={u[0]} />
+  if (guest) {
+    return (
+      <div className="leftBar">
+        <div className="containerTitle">
+          <div className="redirect guest" onClick={() => navigate("/")}>
+            Login to create and view groups
           </div>
-        ))}
-        <div className="showMore">Show more</div>
+        </div>
+
+        <div className="leftBarComponentContainer">
+          <button onClick={getTrendingTags}>Refresh Trending</button>
+          <div className="containerTitle">Trending</div>
+          {TrendingTags.map((u) => (
+            <div className="trending">{u}</div>
+          ))}
+          <div className="redirect guest" onClick={() => navigate("/")}>
+            Login to add an event
+          </div>
+          <div className="leftBarComponentContainer">
+            <button onClick={getEvents}>Refresh Events</button>
+            <div className="containerTitle">Recent Events</div>
+
+            {Events.map((u) => (
+              <div className="event">
+                <div className="eventAvatar">
+                  <Avatar src={u[4]} alt="" />
+                </div>
+                <div className="eventContainerRight">
+                  <div className="eventName">{u[0]}</div>
+                  <div className="eventTime">
+                    {u[1]}
+                    {u[2]}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-      <div className="leftBarComponentContainer">
-        <NewTag />
-        <button onClick={getTrendingTags}>Refresh Trending</button>
-        <div className="containerTitle">Trending</div>
-        {TrendingTags.map((u) => (
-          <div className="trending">{u}</div>
-        ))}
-        <div className="showMore">Show more</div>
-      </div>
-      <div className="leftBarComponentContainer">
-        <button onClick={getEvents}>Refresh Events</button>
-        <div className="containerTitle">Recent Events</div>
-        <AddEvent username={username} />
-        {Events.map((u) => (
-          <div className="event">
-            <div className="eventAvatar">
-              <Avatar src={u[4]} alt="" />
+    );
+  } else {
+    return (
+      <div className="leftBar">
+        <div className="leftBarComponentContainer">
+          <CreateGroup username={username} />
+          <input
+            type="submit"
+            placeholder="refresh feed"
+            onClick={getAllGroups}
+            value="refresh suggestion"
+          />
+          <div className="containerTitle">Your Groups</div>
+          {Groups.map((u) => (
+            <div className="group">
+              <div className="groupAvatar">
+                <Avatar
+                  src={u[1]}
+                  onClick={() => navigate("/group", { state: { group: u[0] } })}
+                />
+              </div>
+              <div className="groupContainerRight">
+                <div className="groupName">{u[0]}</div>
+                <div className="groupDescription">{u[2]}</div>
+              </div>
+
+              <ViewGroup username={username} groupName={u[0]} />
             </div>
-            <div className="eventContainerRight">
-              <div className="eventName">{u[0]}</div>
-              <div className="eventTime">
-                {u[1]}
-                {u[2]}
+          ))}
+          <div className="showMore">Show more</div>
+        </div>
+        <div className="leftBarComponentContainer">
+          <NewTag />
+          <button onClick={getTrendingTags}>Refresh Trending</button>
+          <div className="containerTitle">Trending</div>
+          {TrendingTags.map((u) => (
+            <div className="trending">{u}</div>
+          ))}
+          <div className="showMore">Show more</div>
+        </div>
+        <div className="leftBarComponentContainer">
+          <button onClick={getEvents}>Refresh Events</button>
+          <div className="containerTitle">Recent Events</div>
+          <AddEvent username={username} />
+          {Events.map((u) => (
+            <div className="event">
+              <div className="eventAvatar">
+                <Avatar src={u[4]} alt="" />
+              </div>
+              <div className="eventContainerRight">
+                <div className="eventName">{u[0]}</div>
+                <div className="eventTime">
+                  {u[1]}
+                  {u[2]}
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-        <div className="showMore">Show more</div>
+          ))}
+          <div className="showMore">Show more</div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default LeftBar;
